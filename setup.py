691a4ee380d02537ffac4c2b92e03b62fb79402d
sterbini <guido.sterbini@cern.ch>
2020-05-26 14:30:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='cl2pd',
+setup(name='nx2pd',
       version='0.01',
-      description='TODO',
-      url='https://github.com/sterbini/cl2pd',
+      description='A simple package for NXCALS',
+      url='https://github.com/sterbini/nx2pd',
       author='Guido STERBINI',
       author_email='guido.sterbini@cern.ch',
-      license='MIT',
-      packages=['cl2pd'],
+      license='GNU',
+      packages=['nx2pd'],
       zip_safe=False)
